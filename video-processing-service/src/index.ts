dc42bd1882a4b43b695e3b10a9bcc3345a59a1b9
Ahsan Imam <aimam@achieve.com>
2024-12-03 15:06:41 -0700

    Add video processing

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,10 +1,36 @@
 import express from "express";
 import ffmpeg from "fluent-ffmpeg";
-const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(express.json())
+
+app.post("/process-video", (req, res) => {
+  // Input video file
+  const inputFilePath = req.body.inputFilePath
+  const outputFilePath = req.body.outputFilePath
+
+  console.log(inputFilePath)
+  console.log(outputFilePath)
+
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send('Bad Request: Missing file path')
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions('-vf', 'scale=-1:360') // 360p
+    .on('end', function () {
+      console.log('Processing finished successfully');
+      res.status(200).send('Processing finished successfully');
+    })
+    .on('error', function (err: any) {
+      console.error('An error occurred: ' + err.message);
+      res.status(500).send('An error occurred: ' + err.message);
+    })
+    .save(outputFilePath);
+
+
 });
 
 app.listen(port, () => {
