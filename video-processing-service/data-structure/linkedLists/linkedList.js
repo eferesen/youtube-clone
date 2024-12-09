81e354f92b58a69c4ca128612849420fc6706235
Ahsan Imam <aimam@achieve.com>
2024-12-09 16:25:44 -0700

    Added print method

--- video-processing-service/data-structure/linkedLists/linkedList.js
+++ video-processing-service/data-structure/linkedLists/linkedList.js
@@ -1,51 +1,54 @@
-class Node {
-  constructor() {
-    this.data = null
+class Node { 
+  constructor(data) {
+    this.data = data
     this.next = null
   }
 }
 
-class LinkedList {
-  contructor() {
+class LinkedList { 
+
+  constructor() {
     this.head = null
     this.size = 0
   }
 
   append(data) {
-    // create a new node
+    // start by creating a new node
+    // Check to see if there exist a node
+    // Append to end of node
+    // Increase size
+
     const newNode = new Node(data)
 
-    // Now determine where to add new node
-
-    // Case one: There is no nodes added to the LL
-    if (this.head) {
+    if (!this.head) {
       this.head = newNode
     } else {
-      // Let traverse the array
-      // Go to the end of the array
-      // append the new node
       let current = this.head
-      while (current) {
-        // keep iterating over the array
+      while (current.next) {
         current = current.next
       }
-      // When current becomes
-      // assign the value
       current.next = newNode
     }
-
-    // In either case increase the length
     this.size++
   }
 
   prepend(data) {
-    // create the new node 
+    // start by creating a new node
     const newNode = new Node(data)
-    // Define current head
-    const currentHead = this.head
-    // Add the current head to the new Node
-    newNode.next = currentHead
-    // Point the head to the new node
+    // get the current head
+    let current = this.head
+    current.next = current
+    // set the newNode's next to current head
     this.head = newNode
+    // increment the size
+    this.size++
+  }
+
+  print() {
+    let current = this.head
+    while (current) {
+      console.log(current.data)
+      current = current.next
+    }
   }
 }
